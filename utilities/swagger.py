0af3f7d32e3400271956762df50653071bda5916
irzelindo <irzelindo.salvador@gmail.com>
2025-07-30 15:21:04 +0200

    Update swagger template

--- utilities/swagger.py
+++ utilities/swagger.py
@@ -4,7 +4,7 @@ swagger_template = {
     "info": {
         "title": "OpenLDR API",
         "description": "This is an API for managing OPENLDR repository.",
-        "version": "0.0.2",
+        "version": "0.0.3",
     },
     "securityDefinitions": {
         "Bearer": {
@@ -15,12 +15,12 @@ swagger_template = {
         }
     },
     "security": [{"Bearer": []}],
-    # "host": "dev.openldr.org.mz",  # Uncomment this line to use the development server
+    "host": "dev.openldr.org.mz",  # Uncomment this line to use the development server
     # "host": "api.openldr.org.mz", # Uncomment this line to use the production server
-    "host": "localhost:5000",  # Uncomment this line to use the local server
+    # "host": "localhost:5000",  # Uncomment this line to use the local server
     "basePath": "/",
-    # "schemes": ["https", "http"],  # Uncomment this line to use both HTTP and HTTPS
-    "schemes": ["http", "https"],  # Uncomment this line to use both HTTP and HTTPS
+    "schemes": ["https", "http"],  # Uncomment this line to use both HTTP and HTTPS
+    # "schemes": ["http", "https"],  # Uncomment this line to use both HTTP and HTTPS
     "parameters": {
         "MonthsParameter": {
             "name": "month",
